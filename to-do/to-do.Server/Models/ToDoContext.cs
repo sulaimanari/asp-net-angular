67a86e7afbfc1f88a18bf8c7777b2e637a7afd55
Ari Sulaiman <ari.sulaiman@scs.ch>
2024-06-10 13:51:45 +0200

    more git ignore

---
+++ to-do/to-do.Server/Models/ToDoContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace to_do.Server.Models;
+
+public partial class ToDoContext : DbContext
+{
+    public ToDoContext()
+    {
+    }
+
+    public ToDoContext(DbContextOptions<ToDoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ToDoItem> ToDoItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=BATANGA\\SQL22EXPRESS;Database=ToDo;Integrated Security=true;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ToDoItem>(entity =>
+        {
+            entity.Property(e => e.Id)
+                .HasDefaultValueSql("(newid())")
+                .HasColumnName("ID");
+            entity.Property(e => e.Item).HasColumnType("text");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ to-do/to-do.Server/Models/ToDoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace to_do.Server.Models;
+
+public partial class ToDoItem
+{
+    public Guid Id { get; set; }
+
+    public string Item { get; set; } = null!;
+}
